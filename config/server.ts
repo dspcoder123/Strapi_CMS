9e236daf0ef5a61e9d0ce2e7425db1f095e1b4fa
dspcoder123 <dspcoder123@gmail.com>
2025-10-30 22:50:34 +0530

    fix:added url and proxy configuration in config

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,8 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('RENDER_EXTERNAL_URL', 'https://strapi-cms-ri6l.onrender.com'),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
